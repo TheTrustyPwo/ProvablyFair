b835f207fa494de79348e889d0b8e296f25ed8a9
steeldragon960 <68459550+steeldragon960@users.noreply.github.com>
2022-01-23 17:16:50 +0800

    Fixed printing of server seed hash

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import hmac
 import random
 import string
 
-# Generate a "Provably Fair" number between 1 and 10000
+# Generate a "Provably Fair" number between 1 and n
 
 CLIENT_SEED = "cmY7dvn29lV7WcEi"  # Client editable seed
 SERVER_SEED_LENGTH = 64
-SPECIAL_NUMBER = 429496.7295
+MAX_NUMBER = 10_000
 
 
 def generate(nonce: int):
     serverSeed = ''.join(  # Random 64 character server seed with only lowercase and digits
         random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(SERVER_SEED_LENGTH))
-    print(hashlib.sha256(serverSeed.encode("utf-8")))
+    print("Server Seed Hash:", hashlib.sha256(serverSeed.encode("utf-8")).hexdigest())
     key = "%s:%s:%s" % (nonce, serverSeed, nonce)  # HMAC Secret Key
     client = "%s:%s:%s" % (nonce, CLIENT_SEED, nonce)  # HMAC Client Key
     signature = hmac.new(
@@ -21,7 +21,7 @@ def generate(nonce: int):
         client.encode("utf-8"),
         hashlib.sha512).hexdigest()  # HMAC-SHA512 Hash
     decimal = int(signature[:8], 16)  # Convert first 8 characters to base 16 decimal
-    return min(round(decimal / SPECIAL_NUMBER), 10000)  # To whole number and ensure number <= 10000
+    return min(round(decimal / 429496.7295), 10000)  # To whole number and ensure number <= 10000
 
 
 def main():
